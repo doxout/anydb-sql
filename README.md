67470a96e17e834971f4a3fc7b5e4f972cd68dbb
Gorgi Kosev <gorgi.kosev@gmail.com>
2013-06-11 14:45:44 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# anydb-sql
+
+anydb-sql combines [node-anydb](https://github.com/grncdr/node-any-db)
+and [node-sql](https://github.com/brianc/node-sql) into a single package.
+
+# examples and usage:
+
+Initializing an instance also creates a connection pool. The url argument 
+is the same as in node-anydb
+
+```js
+var anydbsql = require('anydb-sql');
+
+var db = anydbsql({
+    url: 'postgres://user:pass@host:port/database',
+    connections: { min: 2, max: 20 }
+});
+```
+
+Defining a table for that database is the same as in node-sql:
+
+```js
+var user = db.define({
+    name: 'Users',
+    columns: ['id', 'email', 'password']
+});
+```
+
+Queries have the addtional methods:
+* exec(function(err, rows)) - executes the query and calls the callback 
+  with an array of rows
+* get(function(err, row)) - executes the query and returns the first result
+* all - same as exec
+* execWithin(transaction, function(err, rows)) - execute within a transaction
+
+
+Use regular node-sql queries then chain one of the additional
+methods at the end:
+
+```js
+user.where({email: user.email}).get(function(err, users) {
+  // users[0].name, 
+});
+```
+
+Join queries have somewhat different results at the moment.
+The format of the result is the same as with anydb
+
+```js
+user.select(user.name, post.content)
+  .from(user.join(post).on(user.id.equals(post.userId)))
+  .where(post.date.gt(yesterday))
+  .where(user.id.equals(id))
+  .get(function(err, res) {
+    // res['user.name'] and res['post.content']
+  });
+```
+
+Create a transactions and execute queries within it
+
+```js
+var tx = db.begin()
+user.insert({name: 'blah'}).returning(user.id).execWithin(tx);
+user.insert({name: 'bleh'}).returning(user.id).execWithin(tx);
+tx.commit();
+```
+
+Transactions have the same API as anydb tranactions
+
+# licence
+
+MIT
+
+
+
---
+++ anydb-sql.js
@@ -0,0 +1,80 @@
+var anyDB = require('any-db');
+var sql = require('sql');
+var url = require('url');
+
+
+module.exports = function (opt) {
+
+    var pool = anyDB.createPool(opt.url, opt.connections);
+
+    var dialect = url.parse(opt.url).protocol;
+    dialect = dialect.substr(0, dialect.length - 1);
+    sql.setDialect(dialect);
+
+    var self = {};
+
+
+    function extendedTable(table) {
+        var extTable = Object.create(table); // inherit everything from a regular table.
+
+
+        // make query methods return extended queries.
+        ['select', 'from', 'insert', 'update',
+            'delete', 'create', 'drop', 'alter', 'where'].forEach(function (key) {
+                extTable[key] = function () {
+                    return extendedQuery(table[key].apply(table, arguments));
+                }
+            });
+
+
+        // make as return extended tables.
+        extTable.as = function () {
+            return extendedTable(table.as.apply(table, arguments));
+        };
+        return extTable;
+    }
+
+
+    function extendedQuery(query) {
+        var extQuery = Object.create(query);
+
+        var self = extQuery;
+
+        extQuery.execWithin = function (where, fn) {
+            var query = self.toQuery(); // {text, params}
+
+            if (!fn)
+                return where.query(query.text, query.values);
+            else
+                return where.query(query.text, query.values, function (err, res) {
+                    fn && fn(err, res ? res.rows : null);
+                });
+        };
+
+        extQuery.exec = extQuery.execWithin.bind(extQuery, pool);
+
+        extQuery.all = extQuery.exec;
+
+        extQuery.get = function (fn) {
+            return this.exec(function (err, rows) {
+                return fn(err, rows && rows.length ? rows[0] : null);
+            })
+        };
+
+
+        return extQuery;
+    }
+
+
+    self.define = function () {
+        return extendedTable(sql.define.apply(sql, arguments));
+    };
+
+
+    self.close = pool.close.bind(pool);
+    self.begin = pool.begin.bind(pool);
+    self.query = pool.query.bind(pool);
+
+    return self;
+
+};
