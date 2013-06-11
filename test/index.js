40334dabe6cae820bc3cc5d13a44a451772812c6
Gorgi Kosev <gorgi.kosev@gmail.com>
2013-06-11 17:09:49 +0200

    complete test coverage

--- test/index.js
+++ test/index.js
@@ -4,10 +4,9 @@ var anydbsql = require('../anydb-sql');
 
 var path = require('path');
 
-var dburl = path.join(__dirname, 'test.db');
 
 var db = anydbsql({
-  url: 'sqlite3://' + dburl,
+  url: 'sqlite3://', 
   connections: 1
 });
 
@@ -18,20 +17,18 @@ var user = db.define({
 
 test('anydb-sql', function(t) {
 
-  db.query('delete from users;', function(err) {
+  db.query('create table users (id integer primary key, name text);', function(err) {
 
     t.notOk(err, 'creating table failed: ' + err);
 
     t.test('insert exec', function(t) {
-      t.plan(1);
       user.insert({id: 1, name: 'test'}).exec(function(err, res) {
-        t.notOk(err, 'user inserted without errors' + err);
+        t.notOk(err, 'user inserted without errors: ' + err);
         t.end();
       });
     });
 
     t.test('where get', function(t) {
-      t.plan(1);
       user.where({id: 1}).get(function(err, user) {
         t.deepEquals(user, {id: 1, name: 'test'})
         t.end();
@@ -39,19 +36,46 @@ test('anydb-sql', function(t) {
     });
 
     t.test('insert transaction', function(t) {
-      console.log('insert transaction');
       db.begin(function(err, tx){
-        console.log('begin transaction', tx);
         user.insert({id: 2, name: 'test2'}).execWithin(tx);
         user.insert({id: 3, name: 'test3'}).execWithin(tx);
-        console.log('now commiting transaction');
         tx.commit(function(err) {
-          console.log('commit complete');
           t.notOk(err, 'two inserts within transaction');
           t.end();
         });
       });
     });
+
+    t.test('incorrect query', function(t) {
+      user.insert({id: 4, name: 'lol'}).returning(user.id).exec(function(err, results) {
+        t.ok(err, 'incorrect query');
+        t.notOk(results, 'should return null results: ' + results)
+        t.end();
+      });
+    });
+
+    t.test('resultless get', function(t) {
+      user.where({id: 40}).get(function(err, usr) {
+        t.notOk(err, 'has no error');
+        t.notOk(usr, 'should return null results:' + usr);
+        t.end();
+      });
+    });
+
+
+    t.test('table.as and join', function(t) {
+      var userx = user.as('userx');
+      var query = user.from(user.join(userx).on(user.id.equals(userx.id)))
+        .where(userx.id.equals(1))
+        .select(userx.id);
+      console.log(query.toQuery())
+
+      query.get(function(err, res) {
+        console.log(err, res);
+        t.equals(res.id,  1, 'user.id is 1');    
+        t.end();
+      });
+    })
 
     t.test('db.close', function(t) {
       t.plan(1);
