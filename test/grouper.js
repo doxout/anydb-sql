4575ad1b85cedc2fa144b509befafbe9546ca7b4
Gorgi Kosev <gorgi.kosev@gmail.com>
2014-03-10 19:05:58 +0100

    trailing whitespace and fix grouping logic

--- test/grouper.js
+++ test/grouper.js
@@ -16,36 +16,47 @@ t.test('normalizer', function(t) {
     });
 });
 
-var grouperData = require('./lib/grouper-data'); 
+var grouperData = require('./lib/grouper-data');
 
 
 function groupTest(val, key) {
-    //return (/\[\]$/).test(key);
     return grouper.isObject(val);
+    //return grouper.isObject(val) && !/\!\!$/.test(key);
 }
 
 function eqTest(val, key) {
     return (/##$/).test(key);
 }
 
-t.test('grouper', function(t) {
-    var group = grouper.group,
-        clean = grouper.clean;
 
-    t.test('minimal but nested in nongroups', function(t) {
-        var grouped = group(grouperData.mininput, groupTest, eqTest);
-        var ungrouped = clean(grouped, /(\[\]|##)$/);
+var group = grouper.group, clean = grouper.clean;
+
+function grouperTest(desc, inData, outData) {
+    t.test(desc, function(t) {
+        var grouped = group(inData, groupTest, eqTest);
+        var ungrouped = clean(grouped, /(\[\]|##|\{\})$/);
         //console.log(util.inspect(ungrouped, false, 5));
-        t.deepEquals(ungrouped, grouperData.minoutput);
+        t.deepEquals(ungrouped, outData);
         t.end();
     });
-    t.test('deep but no nesting of non-groups', function(t) {
-        var grouped = group(grouperData.input, groupTest, eqTest);
-        var ungrouped = clean(grouped, /(\[\]|##)$/);
-        //console.log(util.inspect(ungrouped, false, 5));
-        t.deepEquals(ungrouped, grouperData.output);
-        t.end();
-    });
-}); 
+}
 
+grouperTest("minimal, nested in nongroups",
+            grouperData.mininput, grouperData.minoutput);
+
+grouperTest("deep but no nesting of non-groups",
+            grouperData.mininput, grouperData.minoutput);
+
+
+var nested = [
+    {'id##': 1, metadata: [{a: 1}, {a: 2}]},
+    {'id##': 2, metadata: [{b: 5}, {b: 8}]}
+];
+var nestedOut = [
+    {'id': 1, metadata: [{a: 1}, {a: 2}]},
+    {'id': 2, metadata: [{b: 5}, {b: 8}]}
+];
+
+
+grouperTest("dont group nested data", nested, nestedOut);
 
